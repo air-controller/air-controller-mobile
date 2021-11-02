b8cb55c258e974bcc6530201f84d7146ba28f25c
Ouyangfeng <626306805@qq.com>
2021-11-02 20:02:33 +0800

    新增文件浏览器实

--- app/src/main/java/com/youngfeng/android/assistant/web/config/AppConfig.kt
+++ app/src/main/java/com/youngfeng/android/assistant/web/config/AppConfig.kt
@@ -1,4 +1,17 @@
 package com.youngfeng.android.assistant.web.config
 
-class AppConfig {
+import android.content.Context
+import android.os.Environment
+import com.yanzhenjie.andserver.annotation.Config
+import com.yanzhenjie.andserver.framework.config.WebConfig
+import com.yanzhenjie.andserver.framework.website.FileBrowser
+import com.yanzhenjie.andserver.framework.website.StorageWebsite
+
+@Config
+class AppConfig : WebConfig {
+
+    override fun onConfig(context: Context, delegate: WebConfig.Delegate?) {
+        // 这里添加SD卡作为访问目录，方便直接访问手机应用上的资源
+        delegate?.addWebsite(FileBrowser(Environment.getExternalStorageDirectory().absolutePath))
+    }
 }
