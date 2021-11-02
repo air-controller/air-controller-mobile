9ad790749f6eb3fff83a4e78c49720c120c72ecb
Ouyangfeng <626306805@qq.com>
2021-11-02 14:01:13 +0800

    新增错误码说明

--- app/src/main/java/com/youngfeng/android/assistant/web/HttpConst.kt
+++ app/src/main/java/com/youngfeng/android/assistant/web/HttpConst.kt
@@ -1,5 +1,11 @@
 package com.youngfeng.android.assistant.web
 
+/**
+ * 错误码由模块加具体错误码，注意不同模块的错误码应该从01开始，到19结束，最多20个错误
+ *
+ * @author Scott Smith 2021/11/2 14:00
+ */
+
 enum class HttpModule(var value: Int) {
     FileModule(1), ImageModule(2), MusicModule(3)
 }
