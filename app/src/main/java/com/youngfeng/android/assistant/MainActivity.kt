555c1b01d1d6d21bfdd955fa1dddf99f9f21c85a
Ouyangfeng <626306805@qq.com>
2021-11-02 11:29:53 +0800

    新增AndServer集成，用于处理Web服务需求

--- app/src/main/java/com/youngfeng/android/assistant/MainActivity.kt
+++ app/src/main/java/com/youngfeng/android/assistant/MainActivity.kt
@@ -2,10 +2,26 @@ package com.youngfeng.android.assistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yanzhenjie.andserver.AndServer
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private val mServer by lazy {
+        AndServer.webServer(this)
+            .port(8080)
+            .timeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mServer.startup()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mServer.shutdown()
     }
 }
