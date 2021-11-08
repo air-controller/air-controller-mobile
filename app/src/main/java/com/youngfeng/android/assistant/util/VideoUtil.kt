8e0d6d1a0f02be0b06eec836f0cba4a35c916b2c
Ouyangfeng <626306805@qq.com>
2021-11-08 18:26:11 +0800

    新增VideoUtil类用于处理视频数据获取相关逻辑

---
+++ app/src/main/java/com/youngfeng/android/assistant/util/VideoUtil.kt
@@ -0,0 +1,11 @@
+package com.youngfeng.android.assistant.util
+
+import android.content.Context
+import com.youngfeng.android.assistant.web.entity.VideoEntity
+
+object VideoUtil {
+
+    fun getAllVideos(context: Context): List<VideoEntity> {
+        throw NotImplementedError()
+    }
+}
---
+++ app/src/main/java/com/youngfeng/android/assistant/web/entity/VideoEntity.kt
@@ -0,0 +1,8 @@
+package com.youngfeng.android.assistant.web.entity
+
+data class VideoEntity(
+    var id: Long,
+    var name: String,
+    var path: String
+) {
+}
