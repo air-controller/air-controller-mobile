ee6ae107cdab853bfff697fb679746a4b7ec55aa
欧阳锋 <ouyangfeng2016@gmail.com>
2021-11-21 18:04:59 +0800

    新增设备搜索逻辑，使用UDP广播实现

--- app/src/main/java/com/youngfeng/android/assistant/manager/DeviceDiscoverManager.kt
+++ app/src/main/java/com/youngfeng/android/assistant/manager/DeviceDiscoverManager.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.net.wifi.WifiManager
 import android.os.Build
+import android.text.format.Formatter
 import com.youngfeng.android.assistant.Constants
 import com.youngfeng.android.assistant.app.MobileAssistantApplication
 import com.youngfeng.android.assistant.model.Device
@@ -12,6 +13,7 @@ import java.net.DatagramSocket
 import java.net.InetSocketAddress
 import java.util.Timer
 import java.util.TimerTask
+import java.util.concurrent.Executors
 
 /**
  * 设备发现管理器，用于发现连接设备，使用UDP广播进行局域网搜索.
@@ -58,40 +60,74 @@ class DeviceDiscoverManagerImpl : DeviceDiscoverManager {
     private var onDeviceDiscover: ((device: Device) -> Unit)? = null
     private val mTimer by lazy { Timer() }
 
+    private val mExecutor by lazy {
+        Executors.newSingleThreadExecutor()
+    }
+
     override fun startDiscover() {
-        if (null == mDatagramSocket) {
-            mDatagramSocket = DatagramSocket(Constants.Port.UDP_DEVICE_DISCOVER)
-        }
-
-        while (!isStarted) {
-            val buffer = ByteArray(1024)
-            val packet = DatagramPacket(buffer, buffer.size)
-            mDatagramSocket?.receive(packet)
-
-            val data = String(buffer)
-            if (isValidData(data)) {
-                print("isValidData: $data")
-            } else {
-                print("It's not valid, data: $data")
+        mExecutor.submit {
+            if (null == mDatagramSocket) {
+                mDatagramSocket = DatagramSocket(Constants.Port.UDP_DEVICE_DISCOVER)
             }
+
+            mTimer.schedule(
+                object : TimerTask() {
+                    override fun run() {
+                        sendBroadcastMsg()
+                    }
+                },
+                0, 1000
+            )
+
+            while (!isStarted) {
+                val buffer = ByteArray(1024)
+                val packet = DatagramPacket(buffer, buffer.size)
+                mDatagramSocket?.receive(packet)
+
+                val data = String(buffer)
+                if (isValidResponse(data)) {
+                    val device = convertToDevice(data)
+                    print("当前设备：${device.name}, platform: ${device.platform}, ip address: ${device.ipAddress}")
+                } else {
+                    print("It's not valid, data: $data")
+                }
+            }
+
+            isStarted = true
+        }
+    }
+
+    private fun convertToDevice(data: String): Device {
+        val realInfo = data.replace("${Constants.SEARCH_RES_PREFIX}${Constants.RADNOM_STR_RES_SEARCH}#", "")
+
+        val arr = realInfo.split("#")
+        var platform = -1
+        if (arr.isNotEmpty()) {
+            platform = arr[0].toIntOrNull() ?: -1
         }
 
-        mTimer.schedule(
-            object : TimerTask() {
-                override fun run() {
-                    sendBroadcastMsg()
-                }
-            },
-            0, 1000
-        )
+        var name = ""
+        if (arr.size > 1) {
+            name = arr[1]
+        }
 
-        isStarted = true
+        var ip = ""
+        if (arr.size > 2) {
+            ip = arr[2]
+        }
+
+        return Device(name, ip, platform)
+    }
+
+    private fun isValidResponse(data: String): Boolean {
+        return data.startsWith("${Constants.SEARCH_RES_PREFIX}${Constants.RADNOM_STR_RES_SEARCH}#")
     }
 
     @SuppressLint("WifiManagerLeak")
     private fun sendBroadcastMsg() {
         val wifiManager = MobileAssistantApplication.getInstance().getSystemService(Context.WIFI_SERVICE) as WifiManager
-        val ip = wifiManager.connectionInfo.ipAddress
+
+        val ip = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
         val name = Build.MODEL
 
         val searchCmd = "${Constants.SEARCH_PREFIX}${Constants
@@ -102,6 +138,7 @@ class DeviceDiscoverManagerImpl : DeviceDiscoverManager {
         val address = InetSocketAddress("255.255.255.255", Constants.Port.UDP_DEVICE_DISCOVER)
         val packet = DatagramPacket(cmdByteArray, cmdByteArray.size, address)
 
+        print("Send msg to 255.255.255.255")
         mDatagramSocket?.send(packet)
     }
 
@@ -120,6 +157,7 @@ class DeviceDiscoverManagerImpl : DeviceDiscoverManager {
     override fun stopDiscover() {
         mDatagramSocket?.close()
         mTimer.cancel()
+        mExecutor.shutdownNow()
         isStarted = false
     }
 }
