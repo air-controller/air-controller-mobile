94de2fa2a03e8220bedddb5e33bda4c1c8f77c6e
Ouyangfeng <626306805@qq.com>
2021-11-26 16:52:07 +0800

    新增接口：获取指定文件夹下视频列表

--- app/src/main/java/com/youngfeng/android/assistant/web/controller/VideoController.kt
+++ app/src/main/java/com/youngfeng/android/assistant/web/controller/VideoController.kt
@@ -1,13 +1,16 @@
 package com.youngfeng.android.assistant.web.controller
 
 import com.yanzhenjie.andserver.annotation.PostMapping
+import com.yanzhenjie.andserver.annotation.RequestBody
 import com.yanzhenjie.andserver.annotation.RequestMapping
 import com.yanzhenjie.andserver.annotation.ResponseBody
 import com.yanzhenjie.andserver.annotation.RestController
 import com.youngfeng.android.assistant.app.MobileAssistantApplication
 import com.youngfeng.android.assistant.util.VideoUtil
 import com.youngfeng.android.assistant.web.entity.HttpResponseEntity
+import com.youngfeng.android.assistant.web.entity.VideoEntity
 import com.youngfeng.android.assistant.web.entity.VideoFolder
+import com.youngfeng.android.assistant.web.request.GetVideosRequest
 
 @RestController
 @RequestMapping("/video")
@@ -22,7 +25,9 @@ class VideoController {
     }
 
     @PostMapping("/list")
-    fun getVideos() {
+    fun getVideos(@RequestBody request: GetVideosRequest): HttpResponseEntity<List<VideoEntity>> {
+        val videos = VideoUtil.getVideosByFolderId(mContext, request.folderId)
+        return HttpResponseEntity.success(videos)
     }
 
     @PostMapping("/delete")
--- app/src/main/java/com/youngfeng/android/assistant/web/entity/VideoEntity.kt
+++ app/src/main/java/com/youngfeng/android/assistant/web/entity/VideoEntity.kt
@@ -3,5 +3,9 @@ package com.youngfeng.android.assistant.web.entity
 data class VideoEntity(
     var id: Long,
     var name: String,
-    var path: String
+    var path: String,
+    var duration: Long,
+    var size: Long,
+    var createTime: Long,
+    var lastModifyTime: Long
 )
