80f86e3ebab29ddaa851402ae37da36d317a788d
欧阳锋 <ouyangfeng2016@gmail.com>
2022-01-31 13:32:44 +0800

    完成首页基础UI

--- app/src/main/java/com/youngfeng/android/assistant/MainActivity.kt
+++ app/src/main/java/com/youngfeng/android/assistant/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.youngfeng.android.assistant
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val mSupportDeveloperText by lazy { findViewById<TextView>(R.id.text_support_developer) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mSupportDeveloperText.paint.flags = Paint.UNDERLINE_TEXT_FLAG
     }
 
     override fun onDestroy() {
